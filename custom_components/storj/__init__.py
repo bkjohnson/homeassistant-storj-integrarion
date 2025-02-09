204ad6bcd77d0757233c04f3c528470aa97bf8ce
Brooks Johnson <brooks.johnson@pm.me>
2025-02-08 16:28:24 -0800

    Add rough draft of integration

--- custom_components/storj/__init__.py
+++ custom_components/storj/__init__.py
@@ -1,111 +1,42 @@
-"""
-Custom integration to integrate Storj with Home Assistant.
+"""Custom integration to integrate Storj with Home Assistant as a backup location."""
 
-For more details about this integration, please refer to
-https://github.com/bkjohnson/homeassistant-storj-integration
-"""
+from __future__ import annotations
 
-import asyncio
-import logging
-from datetime import timedelta
+from collections.abc import Callable
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import Config
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import ConfigEntryNotReady
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
-from homeassistant.helpers.update_coordinator import UpdateFailed
+from homeassistant.helpers import instance_id
+from homeassistant.util.hass_dict import HassKey
 
-from .api import StorjApiClient
-from .const import CONF_PASSWORD
-from .const import CONF_USERNAME
+from .api import StorjClient
 from .const import DOMAIN
-from .const import PLATFORMS
-from .const import STARTUP_MESSAGE
 
-SCAN_INTERVAL = timedelta(seconds=30)
+type StorjConfigEntry = ConfigEntry[StorjClient]
 
-_LOGGER: logging.Logger = logging.getLogger(__package__)
+DATA_BACKUP_AGENT_LISTENERS: HassKey[list[Callable[[], None]]] = HassKey(
+    f"{DOMAIN}.backup_agent_listeners"
+)
 
 
-async def async_setup(hass: HomeAssistant, config: Config):
-    """Set up this integration using YAML is not supported."""
+async def async_setup_entry(hass: HomeAssistant, entry: StorjConfigEntry) -> bool:
+    """Set up storj from a config entry."""
+
+    # TODO 1. Create API instance
+    # TODO 2. Validate the API connection (and authentication)
+    # TODO 3. Store an API object for your platforms to access
+
+    entry.runtime_data = StorjClient(await instance_id.async_get(hass))
+
     return True
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up this integration using UI."""
-    if hass.data.get(DOMAIN) is None:
-        hass.data.setdefault(DOMAIN, {})
-        _LOGGER.info(STARTUP_MESSAGE)
-
-    username = entry.data.get(CONF_USERNAME)
-    password = entry.data.get(CONF_PASSWORD)
-
-    session = async_get_clientsession(hass)
-    client = StorjApiClient(username, password, session)
-
-    coordinator = StorjDataUpdateCoordinator(hass, client=client)
-    await coordinator.async_refresh()
-
-    if not coordinator.last_update_success:
-        raise ConfigEntryNotReady
-
-    hass.data[DOMAIN][entry.entry_id] = coordinator
-
-    for platform in PLATFORMS:
-        if entry.options.get(platform, True):
-            coordinator.platforms.append(platform)
-            hass.async_add_job(
-                hass.config_entries.async_forward_entry_setup(entry, platform)
-            )
-
-    entry.add_update_listener(async_reload_entry)
+async def async_unload_entry(hass: HomeAssistant, entry: StorjConfigEntry) -> bool:
+    """Unload a config entry."""
+    hass.loop.call_soon(_notify_backup_listeners, hass)
     return True
 
 
-class StorjDataUpdateCoordinator(DataUpdateCoordinator):
-    """Class to manage fetching data from the API."""
-
-    def __init__(
-        self,
-        hass: HomeAssistant,
-        client: StorjApiClient,
-    ) -> None:
-        """Initialize."""
-        self.api = client
-        self.platforms = []
-
-        super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=SCAN_INTERVAL)
-
-    async def _async_update_data(self):
-        """Update data via library."""
-        try:
-            return await self.api.async_get_data()
-        except Exception as exception:
-            raise UpdateFailed() from exception
-
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Handle removal of an entry."""
-    coordinator = hass.data[DOMAIN][entry.entry_id]
-    unloaded = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, platform)
-                for platform in PLATFORMS
-                if platform in coordinator.platforms
-            ]
-        )
-    )
-    if unloaded:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unloaded
-
-
-async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Reload config entry."""
-    await async_unload_entry(hass, entry)
-    await async_setup_entry(hass, entry)
+def _notify_backup_listeners(hass: HomeAssistant) -> None:
+    for listener in hass.data.get(DATA_BACKUP_AGENT_LISTENERS, []):
+        listener()
