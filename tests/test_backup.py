a4741ec6818a10d54a6595dbd8da7b78ecf792d0
Brooks Johnson <brooks.johnson@pm.me>
2025-02-08 23:18:34 -0800

    Add test file
    I can't get this to pass locally, maybe it will work in CI.

---
+++ tests/test_backup.py
@@ -0,0 +1,131 @@
+"""Test the Storj BackupAgent"""
+
+from collections.abc import AsyncGenerator
+from io import StringIO
+from homeassistant.core import HomeAssistant
+
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+from pytest_homeassistant_custom_component.typing import ClientSessionGenerator
+from syrupy.assertion import SnapshotAssertion
+from unittest.mock import Mock, patch
+from homeassistant.setup import async_setup_component
+from homeassistant.components.backup import (
+    DOMAIN as BACKUP_DOMAIN,
+    AddonInfo,
+    AgentBackup,
+)
+
+from custom_components.storj.const import DOMAIN
+import pytest
+
+# from .conftest import CONFIG_ENTRY_TITLE, TEST_AGENT_ID
+
+TEST_ACCESS_GRANT = "123xyz"
+TEST_AGENT_ID = "storj.testuser_domain_com"
+CONFIG_ENTRY_TITLE = "Storj entry title"
+
+TEST_AGENT_BACKUP = AgentBackup(
+    addons=[AddonInfo(name="Test", slug="test", version="1.0.0")],
+    backup_id="test-backup",
+    database_included=True,
+    date="2025-01-01T01:23:45.678Z",
+    extra_metadata={
+        "with_automatic_settings": False,
+    },
+    folders=[],
+    homeassistant_included=True,
+    homeassistant_version="2024.12.0",
+    name="Test",
+    protected=False,
+    size=987,
+)
+
+
+async def setup_integration(
+    hass: HomeAssistant, mock_config_entry: MockConfigEntry
+) -> None:
+    """Set up the OneDrive integration for testing."""
+    mock_config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(mock_config_entry.entry_id)
+    await hass.async_block_till_done()
+
+
+# @pytest.fixture
+# def mock_api() -> Generator[MagicMock]:
+#     """Return a mocked StorjClient."""
+#     with patch(
+#         "asyncio.create_subprocess_exec"
+#     ) as mock_api_cl:
+#         mock_api = mock_api_cl.return_value
+#         yield mock_api
+
+
+@pytest.fixture(name="mock_config_entry")
+def mock_config_entry() -> MockConfigEntry:
+    """Fixture for MockConfigEntry."""
+    return MockConfigEntry(
+        domain=DOMAIN,
+        unique_id=TEST_ACCESS_GRANT,
+        title=CONFIG_ENTRY_TITLE,
+        data={"access_grant": TEST_ACCESS_GRANT, "bucket_name": "ha-backups"},
+    )
+
+
+@pytest.fixture(autouse=True)
+async def setup_backup_integration(
+    hass: HomeAssistant,
+    mock_config_entry: MockConfigEntry,
+) -> AsyncGenerator[None]:
+    """Set up onedrive integration."""
+    with (
+        patch("homeassistant.components.backup.is_hassio", return_value=False),
+        patch("homeassistant.components.backup.store.STORE_DELAY_SAVE", 0),
+    ):
+        assert await async_setup_component(hass, BACKUP_DOMAIN, {BACKUP_DOMAIN: {}})
+        await setup_integration(hass, mock_config_entry)
+
+        await hass.async_block_till_done()
+        yield
+
+
+# @pytest.mark.asyncio
+async def test_agents_upload(
+    hass: HomeAssistant,
+    hass_client: ClientSessionGenerator,
+    caplog: pytest.LogCaptureFixture,
+    # mock_api: MagicMock,
+    mock_config_entry: MockConfigEntry,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test agent upload backup."""
+    # mock_api.upload_file = AsyncMock(return_value=None)
+
+    assert await async_setup_component(hass, BACKUP_DOMAIN, {})
+    # await hass.async_block_till_done()
+    client = await hass_client()
+
+    with (
+        patch(
+            "homeassistant.components.backup.manager.BackupManager.async_get_backup",
+        ) as fetch_backup,
+        patch(
+            "homeassistant.components.backup.manager.read_backup",
+            return_value=TEST_AGENT_BACKUP,
+        ),
+        patch("pathlib.Path.open") as mocked_open,
+    ):
+        mocked_open.return_value.read = Mock(side_effect=[b"test", b""])
+        fetch_backup.return_value = TEST_AGENT_BACKUP
+        resp = await client.post(
+            # f"/api/backup/upload?agent_id={TEST_AGENT_ID}",
+            f"/api/backup/upload?agent_id={DOMAIN}.{mock_config_entry.unique_id}",
+            data={"file": StringIO("test")},
+        )
+
+    # print(mock_api)
+    assert resp.status == 201
+    assert f"Uploading backup: {TEST_AGENT_BACKUP.backup_id}" in caplog.text
+    # assert f"Uploaded backup: {TEST_AGENT_BACKUP.backup_id}" in caplog.text
+
+    # mock_api.async_upload_backup.assert_called_once()
+    # assert [tuple(mock_call) for mock_call in mock_api.mock_calls] == snapshot
